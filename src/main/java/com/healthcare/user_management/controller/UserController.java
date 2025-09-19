fc71f4c62e4dbd429b0a4ba184840dfcacf935e4
salaheddinesamid <salaheddine.kobra@iu-study.org>
2025-09-19 22:39:01 +0100

    update

--- src/main/java/com/healthcare/user_management/controller/UserController.java
+++ src/main/java/com/healthcare/user_management/controller/UserController.java
@@ -1,5 +1,9 @@
 package com.healthcare.user_management.controller;
 
+import com.healthcare.user_management.dto.UserResponseDTO;
+import com.healthcare.user_management.service.UserService;
+import com.healthcare.user_management.service.implementation.UserServiceImplementation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +14,18 @@ import java.util.List;
 @RequestMapping("/api/user/")
 public class UserController {
 
+    private final UserServiceImplementation userService;
+
+    @Autowired
+    public UserController(UserServiceImplementation userService) {
+        this.userService = userService;
+    }
+
     /**
      *
      */
     @GetMapping("get_all")
-    public List<User>
+    public List<UserResponseDTO> getUsers(){
+        return userService.getAllUsers();
+    }
 }
--- src/main/java/com/healthcare/user_management/service/implementation/UserServiceImplementation.java
+++ src/main/java/com/healthcare/user_management/service/implementation/UserServiceImplementation.java
@@ -1,4 +1,33 @@
 package com.healthcare.user_management.service.implementation;
 
-public class UserServiceImplementation {
+
+import com.healthcare.user_management.dto.NewUserRequestDTO;
+import com.healthcare.user_management.dto.NewUserResponseDTO;
+import com.healthcare.user_management.dto.UserResponseDTO;
+import com.healthcare.user_management.service.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImplementation implements UserService {
+    @Override
+    public NewUserResponseDTO newUser(NewUserRequestDTO newUserRequestDTO) {
+        return null;
+    }
+
+    @Override
+    public List<UserResponseDTO> getAllUsers() {
+        return List.of();
+    }
+
+    @Override
+    public UserResponseDTO getUser(String email) {
+        return null;
+    }
+
+    @Override
+    public void removeUser(int id) {
+
+    }
 }
