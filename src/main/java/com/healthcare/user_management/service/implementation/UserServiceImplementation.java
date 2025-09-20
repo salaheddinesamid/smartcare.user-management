ba55b00521dd2627a7d3f045d579a07af4134b62
salaheddinesamid <salaheddine.kobra@iu-study.org>
2025-09-20 19:59:04 +0100

    update

--- src/main/java/com/healthcare/user_management/service/implementation/UserServiceImplementation.java
+++ src/main/java/com/healthcare/user_management/service/implementation/UserServiceImplementation.java
@@ -15,10 +15,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collector;
 
 @Service
 public class UserServiceImplementation implements UserService {
