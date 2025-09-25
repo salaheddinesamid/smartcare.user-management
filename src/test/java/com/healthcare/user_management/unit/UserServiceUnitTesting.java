7a1cb841ba865ef797ed1f91bc94e8ac986551b8
salaheddinesamid <salaheddine.kobra@iu-study.org>
2025-09-25 11:11:05 +0100

    add unit test

--- src/test/java/com/healthcare/user_management/unit/UserServiceUnitTesting.java
+++ src/test/java/com/healthcare/user_management/unit/UserServiceUnitTesting.java
@@ -1,4 +1,37 @@
 package com.healthcare.user_management.unit;
 
+import com.healthcare.user_management.repo.UserRepository;
+import com.healthcare.user_management.service.implementation.UserServiceImplementation;
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+/**
+ * This class is used to write unit testing of user service methods
+ */
 public class UserServiceUnitTesting {
+
+    @Mock
+    private UserServiceImplementation userServiceImplementation;
+
+    @Mock
+    private UserRepository userRepository;
+
+
+    @BeforeEach
+    void setUp(){
+        MockitoAnnotations.openMocks(this);
+    }
+
+
+    /**
+     *
+     */
+    @Test
+    void testCreateNewUser(){
+
+    }
 }
